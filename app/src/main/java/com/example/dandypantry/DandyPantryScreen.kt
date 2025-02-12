1f1d4ffb129e9f01b2a719b87a00054283a4ef89
sunseehray <sabtirazona@byui.edu>
2025-02-12 14:42:24 +0800

    navigation file started

--- app/src/main/java/com/example/dandypantry/DandyPantryScreen.kt
+++ app/src/main/java/com/example/dandypantry/DandyPantryScreen.kt
@@ -1,24 +1,75 @@
 package com.example.dandypantry
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.dandypantry.ui.theme.DandyPantryTheme
+
+
+// this is the file for navigating between pages
+// establish routes, etc
 
 @Composable
 fun DandyPantryScreen() {
-    // this is the file for navigating between pages
-    // establish routes, etc
+    Column(
+        modifier = Modifier.padding(16.dp).fillMaxSize()
+    ) {
+        // when app starts
 
-    // when app starts
+        // when app wants to go to the view inventory screen
+        Button(
+            onClick = { }
+        ) {
+            Text(text = "View Inventory")
+        }
 
-    // when app wants to go to the view inventory screen
+        // when app wants to go to the create item screen (C rud)
+        Button(
+            onClick = { }
+        ) {
+            Text(text = "Create")
+        }
 
-    // when app wants to go to the create item screen (C rud)
+        // when app wants to view the item screen (c R ud)
+        Button(
+            onClick = { }
+        ) {
+            Text(text = "Read")
+        }
 
-    // when app wants to view the item screen (c R ud)
+        // when app wants to edit the item screen (cr U d)
+        Button(
+            onClick = { }
+        ) {
+            Text(text = "Update")
+        }
 
-    // when app wants to edit the item screen (cr U d)
+        // when app wants to confirm delete the item screen (cru D)
+        Button(
+            onClick = { }
+        ) {
+            Text(text = "Delete")
+        }
 
-    // when app wants to confirm delete the item screen (cru D)
+        // when app wants to exit
+        Button(
+            onClick = { }
+        ) {
+            Text(text = "Exit")
+        }
+    }
+}
 
-    // when app wants to exit
-
+@Preview(showBackground = true)
+@Composable
+fun DandyPantryScreenPreview() {
+    DandyPantryTheme {
+        DandyPantryScreen()
+    }
 }
--- app/src/main/java/com/example/dandypantry/ui/InventoryScreen.kt
+++ app/src/main/java/com/example/dandypantry/ui/InventoryScreen.kt
@@ -1,5 +1,48 @@
 package com.example.dandypantry.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dandypantry.ui.theme.DandyPantryTheme
+
 // Screen for viewing a list of the inventory
 
 // Each item on the list is a button that redirects to the item's view page - ItemViewScreen
+
+@Composable
+fun InventoryScreen() {
+    Column() {
+        Text(text = "Inventory Screen")
+        Column() {
+            ItemCard("Item 1")
+            ItemCard("Item 2")
+            ItemCard("Item 3")
+        }
+        Column() {
+            Button(
+                onClick = {},
+            ) {
+                Text(text = "BACK")
+            }
+        }
+    }
+
+}
+
+// Row of item with name, quantity, price, and view button
+@Composable
+fun ItemCard(
+    text: String,
+) {
+    Text(text = text)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InventoryScreenPreview() {
+    DandyPantryTheme {
+        InventoryScreen()
+    }
+}
